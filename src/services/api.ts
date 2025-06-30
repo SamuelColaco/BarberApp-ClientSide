bfca41ab264909567a3a234ea34eb5040c040023
Samuel <colaco.samuel8@gmail.com>
2025-06-30 12:40:47 -0300

    Define api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "https://barberapp-api-3jxo.onrender.com"
 })
 
 api.interceptors.request.use((env) => {
